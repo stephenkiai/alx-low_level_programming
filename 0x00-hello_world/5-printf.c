02ac98d21c37cf16f869a2258fefdc9f979248b8
stephenkiai <stephenkiai23@gmail.com>
2023-02-17 02:05:06 +0300

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ *
+ * Descriptions: This function prints a message to the console using the
+ *              printf() function.
+ *
+ * Returns: Always 0 (Success)
+*/
+init main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+
+}
