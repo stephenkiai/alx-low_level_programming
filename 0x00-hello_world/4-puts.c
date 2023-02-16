32b63bd720331bf3f31986afbde773b3e510cf19
stephenkiai <stephenkiai23@gmail.com>
2023-02-17 01:03:20 +0300

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ */
 int main(void)
 {
 /*string initialization*/
-char Mystr[] = "Programming is like building a multilingual puzzle\n";
+char Mystr[] = "Programming is like building a multilingual puzzle\n;
 
 puts(Mystr); /*printing the stdoutput*/
 
