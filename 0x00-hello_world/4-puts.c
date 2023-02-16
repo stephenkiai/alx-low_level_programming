7e81a7e3a973d4d75e2dc1c9e8481caa43979a46
stephenkiai <stephenkiai23@gmail.com>
2023-02-17 00:45:01 +0300

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+/*string initialization*/
+char Mystr[] = "Programming is like building a multilingual puzzle\n";
+
+puts(Mystr); /*printing the stdoutput*/
+
+return (0);
+}
