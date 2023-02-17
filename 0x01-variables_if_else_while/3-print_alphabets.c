2784bdcf869b3e8589075aea1d3b2c4595d987a6
stephenkiai <stephenkiai23@gmail.com>
2023-02-18 00:21:22 +0300

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+  /* Get the character(s) to be written*/
+  getchar ch = 'a..z\n';
+
+  /* printing output*/
+ putchar(ch);
+
+ return (0);
+}
