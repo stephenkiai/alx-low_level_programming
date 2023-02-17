2b8ba0f02d081953081c9eab2814b4f9e7e0d81f
stephenkiai <stephenkiai23@gmail.com>
2023-02-17 03:23:21 +0300

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descriptions:c program that prints "and that piece of art is useful" - Dora   * Korpar, 2015-10-19", followed by a new line, to the standard error.not        * allowed to use any functions listed in the NAME section of the man (3) printf * or man (3) puts
+ *
+ * Returns: Always 1 (Success)
+*/
+int main(void)
+  
