ca78d9e88b2c588b4104a1a2152c045a0a089b83
stephenkiai <stephenkiai23@gmail.com>
2023-02-19 18:24:33 +0300

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,16 @@ int main(void)
 
 if (n > 0)
 {
-printf("The number is positive\n");
+printf("%d is positive\n", n);
 }
 
 else if (n == 0)
 {
-printf("The number is zero\n");
+printf("%d is zero\n", n);
 }
 
 else
-printf("The number is negative\n");
+printf("%d is negative\n", n);
 
 return (0);
 }
