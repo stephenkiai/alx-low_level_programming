78338cc90c6cecb9836fae305ddac634deb5f3fa
stephenkiai <stephenkiai23@gmail.com>
2023-02-22 01:34:59 +0300

    functions and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,13 @@
 #include "main.h"
-
 /**
  * print_alphabet - prints alphabet a-z in lowercase
- *
- * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-char n;
+	char n;
 
-for (n = 'a'; n <= 'z'; n++)
-
-_putchar(n);
-_putchar('\n');
-
- return (0);
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,7 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char C);
 void print_alphabet(void);
+
+#endif
