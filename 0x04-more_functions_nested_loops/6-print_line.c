514fae972cebaced1be66bc5d02dbd35e72c65e4
stephenkiai <stephenkiai23@gmail.com>
2023-02-24 01:03:33 +0300

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * print_line - It takes one argument
+ * @n: length of line
+ *
+ * Return: returns void
+ */
+void print_line(int n)
+{
+int num;
+
+for (num = 0; num < n; num++)
+{
+  _putchar('_');
+}
+_putchar('\n');
+}
