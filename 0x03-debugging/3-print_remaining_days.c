4027f779331dc8265ede3850e24d4c258e1392db
stephenkiai <stephenkiai23@gmail.com>
2023-02-24 02:17:42 +0300

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,29 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 && year % 100 == 0) || (year % 400 == 0))
+if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0))
 {
-if (month >= 2 && day >= 60)
+int leap_days = 366;
+
+if (day >= 60 && month > 2)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", leap_days - day);
 }
 else
 {
-if (month == 2 && day == 60)
+if (day == 60 && month == 2)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
+int standard_days = 365;
+
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", standard_days - day);
 }
 }
 }
