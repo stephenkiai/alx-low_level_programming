07b2ac46ee7651aa3183100ffd91b18a04a6ac08
stephenkiai <stephenkiai23@gmail.com>
2023-02-24 20:34:44 +0300

    more functions and nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,23 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
+printf(" ");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf("Buzz");
+printf(" ");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
+printf(" ");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+printf(" ");
 }
 }
 printf("\n");
