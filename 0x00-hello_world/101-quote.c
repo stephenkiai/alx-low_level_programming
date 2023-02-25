00821b49da945db9ca222bc11e3af40bc1182245
stephenkiai <stephenkiai23@gmail.com>
2023-02-25 15:45:09 +0300

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,12 @@
 /**
  * main - Entry point
  *
- * Descriptions:c program that prints "and that piece of art is useful" - Dora   * Korpar, 2015-10-19", followed by a new line, to the standard error.not        * allowed to use any functions listed in the NAME section of the man (3) printf * or man (3) puts
+ * Descriptions: prints exactly "and that piece of art is useful"
  *
  * Returns: Always 1 (Success)
 */
 int main(void)
-  
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
