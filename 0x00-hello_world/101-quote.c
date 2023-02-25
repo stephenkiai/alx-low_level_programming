7dee850319a96c8a9ba4f046bad2f78fb092bc5c
stephenkiai <stephenkiai23@gmail.com>
2023-02-25 16:04:55 +0300

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Descriptions: prints exactly "and that piece of art is useful"
+ * main - prints exactly "and that piece of art is useful"
  *
  * Returns: Always 1 (Success)
-*/
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
