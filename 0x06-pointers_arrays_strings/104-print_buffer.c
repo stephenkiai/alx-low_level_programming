be03bc3c4b97b7bc82bd05df1509719599500910
stephenkiai <123061554+stephenkiai@users.noreply.github.com>
2023-03-04 01:14:19 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,20 +9,3 @@
  */
 void print_buffer(char *b, int size)
 {
-int i;
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-{
-printf("%08x: ", i * 10);
-if (i < size / 10)
-{
-print_line(b, 9, i);
-}
-else
-{
-print_line(b, size % 10 - 1, i);
-}
-putchar('\n');
-}
-if (size == 0)
-putchar('\n');
-}
