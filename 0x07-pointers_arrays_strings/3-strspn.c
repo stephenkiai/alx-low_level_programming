dccda4f76820511e9ecbd4a3d4899608877bf881
stephenkiai <stephenkiai23@gmail.com>
2023-03-07 01:01:55 +0300

    even more pointers,arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*   _strspn - return length of string that matches values consistently
-*    @s: string to search
-*     @accept: target matches
-*      Return: number of bytes consecutively matched
+* _strspn - return length of string that matches values
+* @s: string 
+* @accept: target matches
+* Return: number of bytes consecutively matched
 */
 
 unsigned int _strspn(char *s, char *accept)
@@ -13,7 +13,6 @@ int j;
 int matches = 0;
 
 while (s[i] != '\0')
-i++;
 {
 
 for (j = 0; accept[j] != '\0'; j++)
