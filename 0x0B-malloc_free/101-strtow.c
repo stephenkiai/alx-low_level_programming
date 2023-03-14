2ae77d72f8026c31dd08eff8b1d522ff80b8880b
stephenkiai <stephenkiai23@gmail.com>
2023-03-15 01:33:24 +0300

    mallocs and free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * wrdcnt - counts the number of words in a string.
+ * @s: string to count.
+ * Return: int of number of words
+ */
+int wrdcnt(char *s)
+{
+
+}
