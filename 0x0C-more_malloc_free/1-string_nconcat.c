93dfd66df850527114258ec9294253de22ac4c28
stephenkiai <stephenkiai23@gmail.com>
2023-03-17 01:04:05 +0300

    more mallocs,calloc,realloc,free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes from s2 to be copied
+ * Return: concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int l1, l2, i, j;
+char *str;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+l1 = l2 = 0;
+
+while (s1[l1] != '\0')
+l1++;
+while (s2[l2] != '\0')
+l2++;
+
+if (n >= l2)
+n = l2;
+
+str = (char *) malloc((l1 + n + 1) * sizeof(char));
+if (str == NULL)
+return (NULL);
+
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
+
+for (j = 0; j < n && s2[j] != '\0'; j++)
+{
+str[i] = s2[j];
+i++;
+}
+str[i] = '\0';
+return (str);
+}
