e124f6df77d44ed72c8eac5445fd30364aed4e45
stephenkiai <stephenkiai23@gmail.com>
2023-03-16 06:00:23 +0300

    argc and argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - prints the minimum number of coins for an amount of money
+* @argc: should count two arguments
+* @argv: arguments given should be program name and amount of money
+* Return: least number of coins, 0 if negative amount, 1 if amount not given
+*/
+int main(int argc, char *argv[])
