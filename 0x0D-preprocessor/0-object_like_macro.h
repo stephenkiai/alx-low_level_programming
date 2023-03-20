96b04ac1ac8ade634c0c9d8ae27a41270d3cda2f
stephenkiai <stephenkiai23@gmail.com>
2023-03-21 02:20:34 +0300

    preprocessor directives/macros

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_OBJECT_LIKE_MACRO_H_
+#ifndef 0_OBJECT_LIKE_MACRO_H
+#define 0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? -(x) : (x))
+
+
+
+#endif
