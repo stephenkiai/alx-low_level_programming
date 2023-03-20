b082b5a06f399fc392630d5ec8e2ca4cb693c15b
stephenkiai <stephenkiai23@gmail.com>
2023-03-21 01:11:05 +0300

    structures/typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+/**
+ *new_dog-creates a new dog.
+ *@name: first member
+ *@age: second age
+ *@owner: third member
+ *return NULL if function fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog-structure for dog.
  * @name: first member
@@ -15,6 +16,7 @@ char *owner;
 };
 
 
+
 /**
  *description-prototypes
  *@name:first member
@@ -24,6 +26,11 @@ char *owner;
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
 
 
 /**
