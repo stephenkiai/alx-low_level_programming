d8f377bcc48aab26fcbefeee9f3f5cbbc96e79e3
stephenkiai <stephenkiai23@gmail.com>
2023-03-20 18:53:06 +0300

    structures/typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,13 +6,11 @@
  *@age: second member
  *@owner: third member
  *@d: pointer to derefernce.
- *
+ *return: void.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
 {
 (*d).name = name;
 (*d).age = age;
 (*d).owner = owner;
-}
 }
