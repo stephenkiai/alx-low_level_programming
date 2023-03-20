31e328c3f0a7aa378e3d3f3ea58124fac21a0b74
stephenkiai <stephenkiai23@gmail.com>
2023-03-21 00:28:52 +0300

    structures/typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,15 @@ char *owner;
 };
 
 /**
- *
- *
- *
+ *function prototypes
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+
+/**
+ *dog_t- defined as a new name for type struct dog.
+ */
+typedef struct dog dog_t;
 
 #endif
