e5b6223b49007b17b63a1d51167861f7988e97e4
stephen kiai <stephenkiai23@gmail.com>
2023-03-22 21:53:56 +0300

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ *print_name- prints a name
+ *@name: char name to be printed
+ *@f: function pointer
+ *return:void 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+{
+f(name);
+}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+**Function Pointers**
+
+**Another application of pointers**
