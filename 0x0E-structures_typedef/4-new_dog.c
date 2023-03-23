53aaca83efdbc3e970777e8c90504fde9d22795a
stephenkiai <stephenkiai23@gmail.com>
2023-03-23 04:26:49 +0300

    structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  *new_dog-creates a new dog.
@@ -8,4 +9,40 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+unsigned int i, namel, ownerl;
+dog_t *dog;
+
+if (name == NULL || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (namel = 0; name[namel]; namel++)
+;
+namel++;
+dog->name = malloc(namel * (sizeof(char)));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (i = 0; i < namel; i++)
+dog->name[i] = name[i];
+
+dog->age = age;
+
+for (ownerl = 0; owner[ownerl]; ownerl++)
+;
+ownerl++;
+dog->owner = malloc(ownerl * (sizeof(char)));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (i = 0; i < ownerl; i++)
+dog->owner[i] = owner[i];
+return (dog);
+}
 
