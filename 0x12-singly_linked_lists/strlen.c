ec2ece7cb15a43502c5dcf0f784b34a3bae29797
stephenkiai <stephenkiai23@gmail.com>
2023-03-31 00:24:51 +0300

    task 2 singly list

---
+++ 0x12-singly_linked_lists/strlen.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *strlen-returns the length of a string
+ *@str: pointer to string
+ *Return: length of string.
+ */
+size_t strlen(const char *str)
+{
+size_t length = 0;
+
+while (*str != '\0')
+{
+length++;
+str++;
+}
+
+return length;
+}
