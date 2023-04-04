729c651cda93d767bc6251d891c5f8e5d201979a
stephenkiai <stephenkiai23@gmail.com>
2023-04-05 00:46:12 +0300

    task 10 singly linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *delete_nodeint_at_index-deletes node at index of a listint_t linked list.
+ *@index: position of node to be deleted
+ *@head: pointer to pointer of head node
+ *Returns: 1 if it succeeds, -1 if it fails.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+unsigned int i;
+listint_t *next, *current;
+
+if (head == NULL || *head == NULL)
+return (-1);
+
+if (index == 0)
+{
+next = (*head)->next;
+free(*head);
+*head = next;
+return (1);
+}
+
+current = *head;
+for (i = 0; i < index - 1; i++)
+{
+if (current->next == NULL)
+return (-1);
+current = current->next;
+}
+next = current->next;
+current->next = next->next;
+free(next);
+return (1);
+}
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ *reverse_listint- reverses a listint_t linked list.
+ *@head: pointer to pointer to head node
+ *
+ *Returns: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+  
