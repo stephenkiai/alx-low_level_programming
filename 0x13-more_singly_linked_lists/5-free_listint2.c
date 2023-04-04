45ceba3c0513e7d894b9888af2d40b694dd66c81
stephenkiai <stephenkiai23@gmail.com>
2023-04-04 19:11:58 +0300

    task 5 & 6 more singly fix

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint2- frees a listint_t list and sets head to NULL.
+ *@head: pointer to pointer to head of list
+ *
+ *
+ */
+void free_listint2(listint_t **head)
+{
+  listint_t *current;
+  if (head ==NULL || *head == NULL)
+    return;
+
+  while (*head != NULL)
+    {
+      current = *head;
+      *head = (*head)->next;
+      free(current);
+    }
+  *head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *pop_listint- deletes the head node of a listint_t linked list
+ *@head: pointer to pointer to head node
+ *
+ *Return:  the head node’s data (n) or (0) if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+int n;
+listint_t *temp;
+if (*head == NULL)
+return (0);
+
+else
+{
+temp = *head;
+n = temp->n;
+*head = (*head)->next;
+free(temp);
+}
+return (n);
+}
