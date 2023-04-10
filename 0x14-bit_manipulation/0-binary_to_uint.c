e18066ee32af67019e28774775aba540b446c3ce
stephen kiai <stephenkiai23@gmail.com>
2023-04-10 20:43:37 +0300

    betty fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,11 +12,11 @@ int i = 0;
 if (b == NULL)
 return (0);/*Return 0 if the input string is null*/
 
-while (b[i] != '\0')/*Loop through until the null terminator is reached*/
+while (b[i] != '\0')/*Loop through until null terminator is reached*/
 {
 if (b[i] != '0' && b[i] != '1')
-return (0);/*returned if contains a character other than '0' or '1*/
-decimal = decimal * 2 + (b[i] - '0');/*Add the current bit to the binary number*/
+return (0);/*returned if contains a character not '0' or '1*/
+decimal = decimal * 2 + (b[i] - '0');/*Add current bit to the binary number*/
 i++;
 }
 return (decimal);
