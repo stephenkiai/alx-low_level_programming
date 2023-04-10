9b6d202efcff91bfaaf50cdffaa54a7327075f6e
stephen kiai <stephenkiai23@gmail.com>
2023-04-10 20:55:44 +0300

    betty fix task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,10 @@
 /**
  *get_endianness-  checks the endianness.
  *Endianness- its the byte order in which a multibyte value is stored in memory
- *union:==user-defined datatype-similar to structure with 1 key diff
+ *union- ==user-defined datatype-similar to structure with 1 key diff
  *in a union, only one data member can hold a value at a time.
  *size of a union is the size of its largest member
- *Returns: 0 if big endian, 1 if little endian.
+ *Return: 0 if big endian, 1 if little endian.
  */
 int get_endianness(void)
 {
@@ -16,5 +16,5 @@ char c[sizeof(int)];   /* Array of 4 characters (1 byte each) */
 } u = {1};            /* Initialize to 1 to set the first byte to 1 */
 
 /* Check the first byte of the integer in the union */
-return u.c[0] == 1;
+return (u.c[0] == 1);
 }
