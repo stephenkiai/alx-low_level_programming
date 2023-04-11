422da9252c8c8f1b0c3049bc6b60a59f1de3e965
stephen kiai <stephenkiai23@gmail.com>
2023-04-11 05:54:52 +0300

    task 3 file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,5 @@
 #include "main.h"
 /**
- * print_error - Prints an error message to standard error.
- * @message: The error message to print.
- */
-void print_error(char *message)
-{
-dprintf(STDERR_FILENO, "Error: %s\n", message);
-}
-
-/**
- * print_usage - Prints the usage message to standard error.
- */
-void print_usage(void)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-}
-
-/**
  * main - Copies the contents of one file to another file.
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
@@ -28,7 +11,7 @@ int fd_from, fd_to, read_count, write_count;
 char buffer[BUFFER_SIZE];
 if (argc != 3)
 {
-print_usage(void);
+print_usage();
 exit(97);
 }
 fd_from = open(argv[1], O_RDONLY);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,13 +13,28 @@
 #include <stdio.h>
 #include <errno.h>
 
+/**
+ * print_error - Prints an error message to standard error.
+ * @message: The error message to print.
+ */
+void print_error(char *message)
+{
+dprintf(STDERR_FILENO, "Error: %s\n", message);
+}
+/**
+ * print_usage - Prints the usage message to standard error.
+ */
+void print_usage()
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+}
 /*prototypes*/
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 size_t strlen(const char *s);
-void print_error(char *message);
-void print_usage(void);
+/*void print_error(char *message);*/
+/*void print_usage(void);*/
 
 #endif
