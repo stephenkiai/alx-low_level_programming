73974ad0978592986c37db4cc45347137e253034
stephen kiai <stephenkiai23@gmail.com>
2023-04-12 14:20:44 +0300

    task 4 file i/o

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,61 @@
 #include "main.h"
+#include <string.h>
+#include <elf.h>
+/**
+ * print_error - Prints an error message to stderr and exits with code 98.
+ * @message: The error message to print.
+ *
+ * Description: This function prints the given error message to stderr and exits
+ * with status code 98.
+ */
+void print_error(char* message)
+{
+fprintf(stderr, "Error: %s\n", message);
+exit(98);
+}
+
+/**
+ * print_header_info - Prints the information contained in the ELF header.
+ * @ehdr: A pointer to the ELF header structure.
+ *
+ * Description: This function prints the information contained in the ELF header
+ * structure in the format specified in the prompt.
+ */
+void print_header_info(Elf64_Ehdr *ehdr)
+{
+int i;
+printf("ELF Header:\n");
+printf("  Magic:   ");
+for (i = 0; i < EI_NIDENT; i++)
+{
+printf("%02x ", ehdr->e_ident[i]);
+}
+printf("\n");
+printf("  Class:                             %s\n", ehdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "Invalid class");
+printf("  Data:                              %s\n", ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "Invalid data encoding");
+printf("  Version:                           %d (current)\n", ehdr->e_ident[EI_VERSION]);
+printf("  OS/ABI:                            %s\n", ehdr->e_ident[EI_OSABI] == 0 ? "UNIX System V ABI" : "Other ABI");
+printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
+printf("  Type:                              %s\n", ehdr->e_type == ET_EXEC ? "EXEC (Executable file)" : ehdr->e_type == ET_DYN ? "DYN (Shared object file)" : "Invalid type");
+printf("  Entry point address:               %lx\n", ehdr->e_entry);
+}
+
+/**
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: This function checks if the given file is an ELF file by
+ * verifying that the first four bytes of the file match the ELF magic number.
+ * If the file is not an ELF file, it prints an error message to stderr and exits
+ * with status code 98.
+ */
+void check_elf(unsigned char *e_ident)
+{
+if (memcmp(e_ident, ELFMAG, SELFMAG) != 0)
+{
+print_error("File is not an ELF file");
+}
+}
 /**
  * main - Reads and displays the information contained in the ELF header
  *        at the start of an ELF file.
@@ -8,71 +65,26 @@
  */
 int main(int argc, char *argv[])
 {
-int fd, i;
-Elf64_Ehdr header;
-
+int fd;
+Elf64_Ehdr ehdr;
+ssize_t num_bytes_read;
 if (argc != 2)
 {
-fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-exit(98);
+print_error("Incorrect number of arguments");
 }
-
 fd = open(argv[1], O_RDONLY);
 if (fd == -1)
 {
-perror("open");
-exit(98);
+print_error("Unable to open file");
 }
-
-if (read(fd, &header, sizeof(header)) != sizeof(header))
+    
+num_bytes_read = read(fd, &ehdr, sizeof(Elf64_Ehdr));
+if (num_bytes_read != sizeof(Elf64_Ehdr))
 {
-perror("read");
-exit(98);
+print_error("Unable to read ELF header");
 }
-
-/* Check if it is an ELF file*/
-if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-header.e_ident[EI_MAG1] != ELFMAG1 ||
-header.e_ident[EI_MAG2] != ELFMAG2 ||
-header.e_ident[EI_MAG3] != ELFMAG3)
-{
-fprintf(stderr, "%s is not an ELF file\n", argv[1]);
-exit(98);
-}
-
-/* Print ELF header information*/
-printf("ELF Header:\n");
-printf("  Magic:   ");
-for (i = 0; i < EI_NIDENT; i++)
-{
-printf("%02x ", header.e_ident[i]);
-}
-printf("\n");
-printf("  Class:                             %s\n",
-header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
-header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
-"Unknown");
-printf("  Data:                              %s\n",
-header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
-"Unknown");
-printf("  Version:                           %d (current)\n",
-header.e_ident[EI_VERSION]);
-printf("  OS/ABI:                            %s\n",
-header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-header.e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - GNU" :
-"Unknown");
-printf("  ABI Version:                       %d\n",
-header.e_ident[EI_ABIVERSION]);
-printf("  Type:                              %s\n",
-header.e_type == ET_REL ? "REL (Relocatable file)" :
-header.e_type == ET_EXEC ? "EXEC (Executable file)" :
-header.e_type == ET_DYN ? "DYN (Shared object file)" :
-header.e_type == ET_CORE ? "CORE (Core file)" :
-"Unknown");
-printf("  Entry point address:               %lx\n",
-header.e_entry);
-
+check_elf(ehdr.e_ident);
+print_header_info(&ehdr);
 close(fd);
 return (0);
 }
