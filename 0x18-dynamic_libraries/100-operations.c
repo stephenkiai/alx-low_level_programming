a4990eaad05f0236dfc71a18f354af831bc94076
stephen kiai <stephenkiai23@gmail.com>
2023-05-23 00:47:45 +0300

    missing c while python coding

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/* add.c */
+/**
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int add(int a, int b){
+return (a+b);
+}
+
+/* sub.c */
+/**
+ * sub - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int sub(int a, int b){
+return (a-b);
+}
+
+/* mul.c */
+/**
+ * mul - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mul(int a, int b){
+return (a*b);
+}
+
+/* div.c */
+/**
+ * div - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int div(int a, int b){
+return (a/b);
+}
+
+/* mod */
+/**
+ * mod - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
